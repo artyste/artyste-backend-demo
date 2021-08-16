161a4cfd0283fac9ac97fa8e1ecd6219f654ac16
dubiella <diogodubiella@gmail.com>
2021-08-15 21:54:38 -0300

    Minting Process

--- api/views.py
+++ api/views.py
@@ -19,4 +19,10 @@ def apiGalleryDetail(requests, slug):
     gallery_get = gallery.objects.get(slug=slug)
     artwork_get = product.objects.filter(gallery=gallery_get)
     serializer = productSerializers(artwork_get, many=True)
-    return Response(serializer.data)
+    new_serializer_data = list(serializer.data)
+    new_serializer_data.append({'gallery': {
+        'name': gallery_get.name,
+        'avatar': gallery_get.imglogo.url,
+        'banner': gallery_get.imgbabner.url,
+    }})
+    return Response(new_serializer_data)
